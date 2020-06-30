aae383c86152501e5097cdfb3ef3252a9dc3d552
unknown <carpediem7760@gmail.com>
2020-06-30 23:08:04 +0900

    router

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+	<div>
+		<header>
+			<router-link to="/login">login</router-link> l
+			<router-link to="/signup">signup</router-link>
+		</header>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
